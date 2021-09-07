33ef2a98dff424f6ed98e78170caf18459caa64d
Kosala Atapattu <Kosala.atapattu@n4l.co.nz>
2021-09-08 11:20:55 +1200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route ("/")
 def hello_world ():
-	return "<H1>Hello, World!!!</H1>"
+	return "<H1>Hello, World 2!!!</H1>"
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8080)
